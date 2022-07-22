21af44abd4bc45eecf085920639e42ebd50a5f6b
Yavishop <pedidos.yavishop@gmail.com>
2022-07-22 01:03:45 +0000

    back

---
+++ back-yavishop/index.js
@@ -0,0 +1,17 @@
+let express = require('express')
+let authRoute = require('./routes/auth')
+let productsRoute = require('./routes/products')
+const cors = require("cors");
+
+let app = express()
+
+app.set('port', process.env.PORT || 3000)
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+
+app.listen(app.get('port'), () => {
+    console.log('Servidor corriendo en el puerto ', app.get('port'))
+})
